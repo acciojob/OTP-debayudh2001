5dbadbd839a267b44e14d7543e53388ecf16283a
debayudh2001 <152640749+debayudh2001@users.noreply.github.com>
2025-02-14 02:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ for(let i=0; i<inputs.length; i++){
 	inputs[i].addEventListener("input", () => {
 		inputs[i+1].focus()
 	})
+	inputs[i].addEventListener("keydown", (e) => {
+		if(e.key === "Backspace"){
+			inputs[i].value = ""
+		}
+	})
 	inputs[i].addEventListener("keyup", (e) => {
 		if(e.key === "Backspace"){
 			inputs[i-1].focus()
+			inputs[i].blur()
 		}
 	})
 }
