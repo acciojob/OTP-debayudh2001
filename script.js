203e769c2ae0b31c073f19b91214ea22756356e5
debayudh2001 <152640749+debayudh2001@users.noreply.github.com>
2025-02-14 01:56:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let inputs = document.querySelectorAll(".code")
+for(let i=0; i<inputs.length; i++){
+	inputs[i].addEventListener("input", () => {
+		inputs[i+1].focus()
+	})
+	inputs[i].addEventListener("keyup", (e) => {
+		if(e.key === "Backspace"){
+			inputs[i-1].focus()
+		}
+	})
+}
